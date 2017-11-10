ca8a71e6b3cb9da8dc16919a520bc06cb04c998a
Yufree <yufree@live.cn>
2017-11-10 14:49:10 -0500

    add deploy

--- _deploy.sh
+++ _deploy.sh
@@ -8,9 +8,13 @@ set -e
 git config --global user.email "yufree@live.cn"
 git config --global user.name "yufree"
 
-git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git book-output
+# clone the repository to the book-output directory
+git clone -b gh-pages \
+  https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git \
+  book-output
 cd book-output
+git rm -rf *
 cp -r ../_book/* ./
-git add --all -- *
-git commit -m "Update the book" || true
-git push origin gh-pages
+git add --all *
+git commit -m"Update the book"
+git push -q origin gh-pages
