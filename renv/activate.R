9565029933a3439afe41a0c41c55f9aab07c2bcd
yufree <yufree@live.cn>
2026-06-21 17:36:48 -0400

    update

--- renv/activate.R
+++ renv/activate.R
@@ -2,8 +2,8 @@
 local({
 
   # the requested version of renv
-  version <- "1.1.5"
-  attr(version, "md5") <- "770fcbc2c4616e8fbcb187cccd46a6b1"
+  version <- "1.2.3"
+  attr(version, "md5") <- "1bd9f58e1cfe27ce035933937c6f03de"
   attr(version, "sha") <- NULL
 
   # the project directory
